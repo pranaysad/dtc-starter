d81a51dced2999d34076825f9472c3483c7ee093
pranaysaani <pranaysaani@gmail.com>
2026-06-13 04:20:14 -0700

    added Google Analytics G-CG6MER6SZM

--- apps/storefront/src/app/layout.tsx
+++ apps/storefront/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
+import Script from "next/script"
 import "styles/globals.css"
+
+const GA_MEASUREMENT_ID = "G-CG6MER6SZM"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
@@ -10,6 +13,18 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
         <main className="relative">{props.children}</main>
       </body>
     </html>
